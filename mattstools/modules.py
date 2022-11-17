fixed masking on IterativeNormLayer

mattstools/modules.py
@@ -557,17 +557,33 @@ class IterativeNormLayer(nn.Module):
         """Applies the standardisation to a batch of inputs, also uses the inputs to
         update the running stats if in training mode"""
         with T.no_grad():
-            inpt = self._mask(inpt, mask)
+            sel_inpt = self._mask(inpt, mask)
             if not self.frozen and self.training:
-                self.update(inpt)
+                self.update(sel_inpt)
 
             # Apply the mapping [None] performs an unsqueeze, broadcasting across batch
-            return (inpt - self.means) / (self.vars.sqrt() + 1e-8)
+            normed_inpt = (sel_inpt - self.means) / (self.vars.sqrt() + 1e-8)
+
+            # Undo the masking
+            if mask is not None:
+                inpt = inpt.clone() # prevents inplace operation, bad for autograd
+                inpt[mask] = normed_inpt
+                return inpt
+
+            return normed_inpt
 
     def reverse(self, inpt: T.Tensor, mask: Optional[T.BoolTensor] = None) -> T.Tensor:
         """Unnormalises the inputs given the recorded stats"""
-        inpt = self._mask(inpt, mask)
-        return inpt * self.vars.sqrt() + self.means
+        sel_inpt = self._mask(inpt, mask)
+        unnormed_inpt = sel_inpt * self.vars.sqrt() + self.means
+
+        # Undo the masking
+        if mask is not None:
+            inpt = inpt.clone() # prevents inplace operation, bad for autograd
+            inpt[mask] = unnormed_inpt
+            return inpt
+
+        return unnormed_inpt
 
     def update(self, inpt: T.Tensor, mask: Optional[T.BoolTensor] = None) -> None:
         """Update the running stats using a batch of data"""

mattstools/torch_utils.py
@@ -118,19 +118,21 @@ def base_modules(module: nn.Module) -> list:
     return total
 
 
-def empty_0dim_like(tensor: T.Tensor) -> T.Tensor:
+def empty_0dim_like(inpt: Union[T.Tensor, np.ndarray]) -> Union[T.Tensor, np.ndarray]:
     """Returns an empty tensor with similar size as the input but with its final
     dimension reduced to 0
     """
 
-    ## Get all but the final dimension
-    all_but_last = tensor.shape[:-1]
+    # Get all but the final dimension
+    all_but_last = inpt.shape[:-1]
 
-    ## Ensure that this is a tuple/list so it can agree with return syntax
+    # Ensure that this is a tuple/list so it can agree with return syntax
     if isinstance(all_but_last, int):
         all_but_last = [all_but_last]
 
-    return T.empty((*all_but_last, 0), dtype=tensor.dtype, device=tensor.device)
+    if isinstance(inpt, T.Tensor):
+        return T.empty((*all_but_last, 0), dtype=inpt.dtype, device=inpt.device)
+    return np.empty((*all_but_last, 0))
 
 
 def get_nrm(name: str, outp_dim: int) -> nn.Module:
@@ -248,6 +250,9 @@ def get_sched(
     ## Pop off the name and learning rate for the optimiser
     dict_copy = sched_dict.copy()
     name = dict_copy.pop("name")
+
+    ## Get the max_lr from the optimiser if not specified
+    max_lr = max_lr or opt.defaults["lr"]
 
     ## Exit if the name indicates no scheduler
     if name in ["", "none", "None"]: