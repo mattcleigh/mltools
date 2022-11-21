32a2837adbd60976d4f1f73718884ec42a44fd30
Matthew Leigh <matthew.leigh@cern.ch>
2022-11-21 23:13:47 +0100

    added runge-kutta diffusion sampler

--- mattstools/diffusion.py
+++ mattstools/diffusion.py
@@ -131,6 +131,18 @@ def cosine_beta_shedule(
     return 2 * (end_angle - start_angle) * T.tan(diffusion_angles)
 
 
+def ddim_predict(
+    noisy_data: T.Tensor,
+    pred_noises: T.Tensor,
+    signal_rates: T.Tensor,
+    noise_rates: T.Tensor,
+) -> T.Tensor:
+    """Use a single ddim step to predict the final image from anywhere in the diffusion
+    process
+    """
+    return (noisy_data - noise_rates * pred_noises) / signal_rates
+
+
 @T.no_grad()
 def ddim_sampler(
     model,
@@ -170,14 +182,12 @@ def ddim_sampler(
     all_stages = []
     step_size = 1 / n_steps
 
-    # Do the very first step of the iteration using pure noise
+    # The initial variables needed for the loop
     next_noisy_data = initial_noise
     next_diff_times = T.ones(num_samples, device=model.device)
     next_signal_rates, next_noise_rates = diff_sched(
         next_diff_times.view(expanded_shape),
     )
-
-    # Cycle through the remainder of all the steps
     for step in tqdm(range(n_steps), "DDIM-sampling", leave=False):
 
         # Update with the previous 'next' step
@@ -221,23 +231,7 @@ def euler_maruyama_sampler(
     ctxt: Optional[T.BoolTensor] = None,
     clip_predictions: Optional[tuple] = None,
 ) -> Tuple[T.Tensor, list]:
-    """Apply the full reverse process to noise to generate a batch of samples
-
-    Args:
-        model: A denoising diffusion model
-            Requires: inpt_dim, device, forward() method that outputs pred noise
-        diif_sched: A diffusion schedule object to calculate signal and noise rates
-        initial_noise: The initial noise to pass through the process
-            If none it will be generated here
-        n_steps: The number of iterations to generate the samples
-        keep_all: Return all stages of diffusion process
-            Can be memory heavy for large batches
-        num_samples: How many samples to generate
-            Ignored if initial_noise is provided
-        mask: The mask for the output point clouds
-        ctxt: The context tensor for the output point clouds
-        clip_predictions: Can stabalise generation by clipping the outputs
-    """
+    """Apply the full reverse process to noise to generate a batch of samples"""
 
     # Get the initial noise for generation and the number of sammples
     num_samples = initial_noise.shape[0]
@@ -249,14 +243,12 @@ def euler_maruyama_sampler(
     all_stages = []
     delta_t = 1 / n_steps
 
-    # Do the very first step of the iteration using pure noise
+    # The initial variables needed for the loop
     x_t = initial_noise
     t = T.ones(num_samples, device=model.device)
-
-    # Cycle through the remainder of all the steps
     for step in tqdm(range(n_steps), "Euler-Maruyama-sampling", leave=False):
 
-        # Apply the denoise step to get X_0 and expected noise
+        # Use the model to get the expected noise
         pred_noises = model(x_t, t, mask, ctxt)
 
         # Use to get s_theta
@@ -291,23 +283,7 @@ def euler_sampler(
     ctxt: Optional[T.BoolTensor] = None,
     clip_predictions: Optional[tuple] = None,
 ) -> Tuple[T.Tensor, list]:
-    """Apply the full reverse process to noise to generate a batch of samples
-
-    Args:
-        model: A denoising diffusion model
-            Requires: inpt_dim, device, forward() method that outputs pred noise
-        diif_sched: A diffusion schedule object to calculate signal and noise rates
-        initial_noise: The initial noise to pass through the process
-            If none it will be generated here
-        n_steps: The number of iterations to generate the samples
-        keep_all: Return all stages of diffusion process
-            Can be memory heavy for large batches
-        num_samples: How many samples to generate
-            Ignored if initial_noise is provided
-        mask: The mask for the output point clouds
-        ctxt: The context tensor for the output point clouds
-        clip_predictions: Can stabalise generation by clipping the outputs
-    """
+    """Apply the full reverse process to noise to generate a batch of samples"""
 
     # Get the initial noise for generation and the number of sammples
     num_samples = initial_noise.shape[0]
@@ -319,23 +295,13 @@ def euler_sampler(
     all_stages = []
     delta_t = 1 / n_steps
 
-    # Do the very first step of the iteration using pure noise
+    # The initial variables needed for the loop
     x_t = initial_noise
     t = T.ones(num_samples, device=model.device)
-
-    # Cycle through the remainder of all the steps
     for step in tqdm(range(n_steps), "Euler-sampling", leave=False):
 
-        # Apply the denoise step to get X_0 and expected noise
-        pred_noises = model(x_t, t, mask, ctxt)
-
-        # Use to get s_theta
-        _, noise_rates = diff_sched(t.view(expanded_shape))
-        s = - pred_noises / noise_rates
-
-        # Take one step using the eu method
-        betas = diff_sched.get_betas(t.view(expanded_shape))
-        x_t += 0.5 * betas * (x_t + s) * delta_t
+        # Take a step using the euler method and the gradient calculated by the ode
+        x_t += get_ode_gradient(model, diff_sched, x_t, t, mask, ctxt) * delta_t
         t -= delta_t
 
         # Keep track of the diffusion evolution
@@ -349,13 +315,64 @@ def euler_sampler(
     return x_t, all_stages
 
 
-def ddim_predict(
-    noisy_data: T.Tensor,
-    pred_noises: T.Tensor,
-    signal_rates: T.Tensor,
-    noise_rates: T.Tensor,
-) -> T.Tensor:
-    """Use a single ddim step to predict the final image from anywhere in the diffusion
-    process
-    """
-    return (noisy_data - noise_rates * pred_noises) / signal_rates
+@T.no_grad()
+def runge_kutta_sampler(
+    model,
+    diff_sched: DiffusionSchedule,
+    initial_noise: T.Tensor,
+    n_steps: int = 50,
+    keep_all: bool = False,
+    mask: Optional[T.Tensor] = None,
+    ctxt: Optional[T.BoolTensor] = None,
+    clip_predictions: Optional[tuple] = None,
+) -> Tuple[T.Tensor, list]:
+    """Apply the full reverse process to noise to generate a batch of samples"""
+
+    # Get the initial noise for generation and the number of sammples
+    num_samples = initial_noise.shape[0]
+
+    # The shape needed for expanding the time encodings
+    expanded_shape = [-1] + [1] * (initial_noise.dim() - 1)
+
+    # Check the input argument for the n_steps, must be less than what was trained
+    all_stages = []
+    delta_t = 1 / n_steps
+
+    # Wrap the ode gradient in a lambda function depending only on xt and t
+    ode_grad = lambda t, x_t : get_ode_gradient(model, diff_sched, x_t, t, mask, ctxt)
+
+    # The initial variables needed for the loop
+    x_t = initial_noise
+    t = T.ones(num_samples, device=model.device)
+    for step in tqdm(range(n_steps), "Runge-Kutta-sampling", leave=False):
+
+        k1 = delta_t * (ode_grad(t, x_t))
+        k2 = delta_t * (ode_grad((t-delta_t/2), (x_t+k1/2)))
+        k3 = delta_t * (ode_grad((t-delta_t/2), (x_t+k2/2)))
+        k4 = delta_t * (ode_grad((t-delta_t), (x_t+k3)))
+        k = (k1+2*k2+2*k3+k4)/6
+        x_t += k
+        t -= delta_t
+
+        # Keep track of the diffusion evolution
+        if keep_all:
+            all_stages.append(x_t)
+
+        # Clamp the predicted X_0 for stability
+        if clip_predictions is not None:
+            x_t.clamp_(*clip_predictions)
+
+    return x_t, all_stages
+
+def get_ode_gradient(
+    model,
+    diff_sched: DiffusionSchedule,
+    x_t: T.Tensor,
+    t: T.Tensor,
+    mask: Optional[T.BoolTensor] = None,
+    ctxt: Optional[T.Tensor] = None,
+) -> None:
+    expanded_shape = [-1] + [1] * (x_t.dim() - 1)
+    _, noise_rates = diff_sched(t.view(expanded_shape))
+    betas = diff_sched.get_betas(t.view(expanded_shape))
+    return 0.5 * betas * (x_t - model(x_t, t, mask, ctxt) / noise_rates)
