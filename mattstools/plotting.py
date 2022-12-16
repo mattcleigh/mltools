Fixed underflow behaviour for multihist

mattstools/plotting.py
@@ -226,7 +226,7 @@ def plot_multi_hists(
     hist_kwargs: dict = None,
     hist_scale: float = 1,
     incl_overflow: bool = False,
-    incl_underflow: bool = True,
+    incl_underflow: bool = False,
     do_step: bool = True,
     do_ratio_to_first: bool = False,
     as_pdf: bool = False,
@@ -327,6 +327,10 @@ def plot_multi_hists(
             ## For a multiple histogram
             if multi_hist is not None and multi_hist[j] > 1:
                 data = np.copy(data_list[j][:, i]).reshape(-1, multi_hist[j])
+                if incl_overflow:
+                    data = np.minimum(data, b[-1])
+                if incl_underflow:
+                    data = np.maximum(data, b[0])
                 mh_hists = []
                 for mh in range(multi_hist[j]):
                     mh_hists.append(np.histogram(data[:, mh], b, density=normed)[0])