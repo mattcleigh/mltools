687d5623ea836b5d33fdaa8dd17dbdfb30eda305
Matthew Leigh <mleigh@cern.ch>
2023-02-24 09:56:37 +0100

    hist ylim bottom unchanged

--- mattstools/plotting.py
+++ mattstools/plotting.py
@@ -446,7 +446,7 @@ def plot_multi_hists(
             setylim = ylim
             axes[i, 0].set_ylim(*setylim)
         else:
-            ylim1, ylim2 = axes[i, 0].get_ylim()
+            _, ylim2 = axes[i, 0].get_ylim()
             if logy:
                 # pad up the ylim (which is in logscale) by 25%
                 ylim2 = 10 ** (np.log10(ylim2) * 1.40)
@@ -454,7 +454,7 @@ def plot_multi_hists(
             else:
                 ylim2 = ylim2 * 1.35
                 setylim = (0, ylim2)
-            axes[i, 0].set_ylim(*setylim)
+            axes[i, 0].set_ylim(top=ylim2)
 
         if do_ratio_to_first:
             axes[i, 1].set_xlim(b[0], b[-1])
