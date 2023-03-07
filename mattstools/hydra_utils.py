f66e6b3cd11075bd93d21967ff8d875c9df48e45
Matthew Leigh <matthew.leigh@cern.ch>
2023-03-07 14:21:45 +0100

    fixed reloading

--- mattstools/hydra_utils.py
+++ mattstools/hydra_utils.py
@@ -22,21 +22,18 @@ log = logging.getLogger(__name__)
 def reload_original_config(cfg: OmegaConf, get_best: bool = False) -> OmegaConf:
     """Replaces the cfg with the one stored at the checkpoint location.
 
-    Will also set the chkpt_dir to the latest version of the 'last'
-    checkpoint
+    Will also set the chkpt_dir to the latest version of the last or
+    best checkpoint
     """
 
     # Load the original config found in the the file directory
     orig_cfg = OmegaConf.load(Path("full_config.yaml"))
 
     # Get the latest updated checkpoint with the prefix last or best
-    if get_best:
-        flag = "best" if get_best else "last"
-        orig_cfg.ckpt_path = str(
-            sorted(Path.cwd().glob(f"checkpoints/{flag}*.ckpt"), key=os.path.getmtime)[
-                -1
-            ]
-        )
+    flag = "best" if get_best else "last"
+    orig_cfg.ckpt_path = str(
+        sorted(Path.cwd().glob(f"checkpoints/{flag}*.ckpt"), key=os.path.getmtime)[-1]
+    )
 
     # Set the wandb logger to attempt to resume the job
     if hasattr(orig_cfg, "loggers"):
