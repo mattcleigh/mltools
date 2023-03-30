704556678b0daef3abad45b81dfa315c65a313a9
Matthew Leigh <matthew.leigh@cern.ch>
2023-03-30 17:32:50 +0200

    fixed one hot encode

--- mattstools/numpy_utils.py
+++ mattstools/numpy_utils.py
@@ -4,8 +4,14 @@ import numpy as np
 
 
 def onehot_encode(
-    a: np.ndarray, max_idx: None | int = None, dtype: np.dtype = np.float32
+    a: np.ndarray,
+    max_idx: None | int = None,
+    dtype: np.dtype = np.float32,
+    count_unique: bool = True,
 ) -> np.ndarray:
+    if count_unique:
+        unique, inverse = np.unique(a, return_inverse=True)
+        a = inverse
     max_idx = max_idx or a.max()
     ncols = max_idx + 1
     out = np.zeros((a.size, ncols), dtype=dtype)
